c2dc4b8804fe016e922e88a30d2d528c79cc7346
Vishnu Rao <79438943+vishnugrao@users.noreply.github.com>
2025-03-16 00:44:13 +1030

    Essential macros calculated, db tables ready to be populated

--- basic/app/dashboard/components/QuantitativeNutrition.tsx
+++ basic/app/dashboard/components/QuantitativeNutrition.tsx
@@ -8,6 +8,8 @@ export default function QuantitativeNutrition(props: {userDetails: User, goalDet
     const { goalDetails } = props;
     const [tdee, setTDEE] = useState(0);
     const [offset, setOffset] = useState(0);
+    const [protein, setProtein] = useState(0);
+    const [fat, setFat] = useState(0);
     
     useEffect(() =>  {
         const bmrConstant = userDetails.gender == "Male" ? 5 : -161;
@@ -17,20 +19,30 @@ export default function QuantitativeNutrition(props: {userDetails: User, goalDet
 
     useEffect(() => {
         if (goalDetails.goal == "Bulk") {
-            setOffset(500);
+            setOffset(0.15 * tdee);
+            setProtein(1.8 * userDetails.weight);
+            setFat(Math.round((0.25 * (tdee + offset)) / 9));
         }
         if (goalDetails.goal == "Shred") {
-            setOffset(-500);
+            setOffset(-0.2 * tdee);
+            setProtein(2 * userDetails.weight);
+            setFat(Math.round((0.21 * (tdee + offset)) / 9));
         }
         if (goalDetails.goal == "Recomp") {
             setOffset(0);
+            setProtein(1.9 * userDetails.weight);
+            setFat(Math.round((0.23 * (tdee + offset))/9));
         }
-    }, [goalDetails.goal])
+    }, [goalDetails.goal, tdee, userDetails.weight, offset])
 
     return (
-        <div className="flex w-1/3 flex-col gap-4">
-            <p className="text-2xl">Total Daily Energy Expenditure (TDEE):&nbsp;{tdee}</p>
-            <p className="text-2xl">Daily Target:&nbsp;{tdee + offset}</p>
-        </div>
+        <>
+            <div className="flex w-1/3 flex-col gap-4">
+                <p className="text-2xl">Total Daily Energy Expenditure (TDEE):&nbsp;{tdee}</p>
+                <p className="text-2xl">Daily Target:&nbsp;{tdee + offset}</p>
+                <p className="text-2xl">Protein Target:&nbsp;{protein}g</p>
+                <p className="text-2xl">Fat Target:&nbsp;{fat}g</p>
+            </div>
+        </>
     );
 }
